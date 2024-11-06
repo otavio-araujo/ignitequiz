ae049e8586cd04830f1fc688c7557d0286d4230f
Otávio Araújo <otavio_araujo@hotmail.com>
2024-11-05 23:34:06 -0300

    Tocando som

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -30,6 +30,7 @@ import Animated, {
 import { GestureDetector, Gesture } from "react-native-gesture-handler"
 import { ProgressBar } from "../../components/ProgressBar"
 import { THEME } from "../../styles/theme"
+import { Audio } from "expo-av"
 
 interface Params {
   id: string
@@ -75,6 +76,17 @@ export function Quiz() {
   const route = useRoute()
   const { id } = route.params as Params
 
+  async function playSound(isCorrect: boolean) {
+    const file = isCorrect
+      ? require("../../assets/correct.mp3")
+      : require("../../assets/wrong.mp3")
+
+    const { sound } = await Audio.Sound.createAsync(file, { shouldPlay: true })
+
+    await sound.setPositionAsync(0)
+    await sound.playAsync()
+  }
+
   function handleSkipConfirm() {
     Alert.alert("Pular", "Deseja realmente pular a questão?", [
       { text: "Sim", onPress: () => handleNextQuestion() },
@@ -111,10 +123,12 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
+      await playSound(true)
       setStatusReply(1)
       setPoints((prevState) => prevState + 1)
       handleNextQuestion()
     } else {
+      await playSound(false)
       setStatusReply(2)
       shakeAnimation()
     }
